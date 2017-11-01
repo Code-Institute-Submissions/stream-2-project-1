054905f58314c899d6864f2ddd65aebe83876f75
Ayse Kubra <aysekubrakupeli@gmail.com>
2017-11-01 16:02:43 +0000

    Most charts working

--- js/javascript.js
+++ js/javascript.js
@@ -1,28 +1,43 @@
  queue()
-        .defer(d3.csv, "data/gtd1970_1975.csv")
+        .defer(d3.csv, "data/gtd.csv")
         .await(makeGraphs);
     function makeGraphs(error, countryData) {
-        
-        // Barchart
         var ndx = crossfilter(countryData);
-        var country_dim = ndx.dimension(dc.pluck('region_txt'));
-        var total_terrorist_acts = country_dim.group();
-        dc.barChart("#first-chart")
-            .width(2000)
-            .height(500)
-            .margins({top: 10, right: 50, bottom: 30, left: 50})
-            .dimension(country_dim)
-            .group(total_terrorist_acts)
-            .transitionDuration(500)
-            .x(d3.scale.ordinal())
-            .xUnits(dc.units.ordinal)
-            .xAxisLabel("Country")
-            .yAxis().ticks(4);
-            
-            
-        // PieChart    
+        // var parseDate = d3.time.format("%m/%d/%Y").parse;
+        // countryData.forEach(function(d){
+        //     d.iyear = parseDate(d.iyear);
+        // });
+        var yearDim = ndx.dimension(function(d){
+            return d.iyear;
+        });
         
-        var attack_type_dim = ndx.dimension(dc.pluck('attacktype1_txt'));
+        var minDate = yearDim.bottom(1)[0].iyear;
+        var maxDate = yearDim.top(1)[0].iyear;
+        
+       
+// ---------------------------------------------------------------------------------------------------------------------------        
+
+        // Number Display
+        var allKilled = ndx.groupAll().reduceSum(dc.pluck('nkill'))
+        var numberKilled = dc.numberDisplay("#number-display");
+            numberKilled
+                .formatNumber(d3.format("nkill"))
+                .valueAccessor(function (d) {
+                      return d;
+                  })
+                .group(allKilled);
+        
+        
+// ---------------------------------------------------------------------------------------------------------------------------                
+        
+        // Lethal - non lethal PieChart    
+        
+        var attack_type_dim = ndx.dimension(function(d) {
+            if(d['nkill'] == 0)
+                return 'Non-lethal';
+            else
+                return 'Lethal';
+        });
         var attack_type = attack_type_dim.group();
         dc.pieChart('#second-chart')
             .height(330)
@@ -31,72 +46,100 @@
             .dimension(attack_type_dim)
             .group(attack_type);
         
+// ---------------------------------------------------------------------------------------------------------------------------        
+        // attackers number killed    
         
-        // PieChart    
-        
-        var attack_type_dim = ndx.dimension(dc.pluck('gname'));
-        var attack_type = attack_type_dim.group();
-        dc.pieChart('#third-chart')
+        var dim = ndx.dimension(dc.pluck('gname'));
+
+        var group = dim.group().reduceSum(dc.pluck('nkill'));
+
+        var chart = dc.rowChart("#third-chart");
+
+        chart
+            .width(600)
             .height(330)
-            .radius(2000)
-            .transitionDuration(1500)
-            .dimension(attack_type_dim)
-            .group(attack_type);
+            .dimension(dim)
+            .cap(4)
+            .group(group)
+            .xAxis().ticks(4);
+
+
+// ---------------------------------------------------------------------------------------------------------------------------
+        //Composite Line Chart
+        
+        var ammountAttacks = yearDim.group()
+        var ammountKilled = yearDim.group().reduceSum(dc.pluck('nkill'))
+
         
         
+        var compositeChart = dc.compositeChart('#composite-chart');
+        compositeChart
+            .width(990)
+            .height(200)
+            .dimension(yearDim)
+            .x(d3.time.scale().domain([minDate, maxDate]))
+            .yAxisLabel("The Y Axis")
+            .legend(dc.legend().x(80).y(20).itemHeight(13).gap(5))
+            .renderHorizontalGridLines(true)
+            .compose([
+                dc.lineChart(compositeChart)
+                    .colors('red')
+                    .group(ammountAttacks, 'Attacks'),
+                dc.lineChart(compositeChart)
+                    .colors('blue')
+                    .group(ammountKilled, 'People Killed'),
+            ])
+            .brushOn(false)
+            .render();
+
+        
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------        
+        //Average killed by way Killed
+        
+        var weapon_dim = ndx.dimension(dc.pluck('weaptype1_txt'))
+    
+         var people_killed_group = weapon_dim.group().reduce(
+            function (p, v) {
+                ++p.count;
+                p.total += v.nkill;
+                p.average = p.total / p.count;
+                return p;
+            },
+            function (p, v) {
+                --p.count;
+                if(p.count == 0) {
+                    p.total = 0;
+                    p.average = 0;
+                } else {
+                    p.total -= v.nkill;
+                    p.average = p.total / p.count;
+                };
+                return p;
+            },
+            function () {
+                return {count: 0, total: 0, average: 0};
+            }
+        );
         
         
-        
-        
-        
-        // // CompositeLine Chart
-        
-        // function makeGraphs(error, countryData) {
-        // var ndx = crossfilter(countryData);
-        // var parseDate = d3.time.format("%d/%m/%Y").parse;
-        // countryData.forEach(function(d){
-        //     d.date = parseDate(d.date);
-        // });
-        // var date_dim = ndx.dimension(dc.pluck('date'));
-        // var minDate = date_dim.bottom(1)[0].date;
-        // var maxDate = date_dim.top(1)[0].date;
-        
-        // var tomSpendByMonth = spendByMonth('Tom');
-        // var bobSpendByMonth = spendByMonth('Bob');
-        // var aliceSpendByMonth = spendByMonth('Alice');
-        
-        // function spendByMonth(name) {
-        //     return date_dim.group().reduceSum(function (d) {
-        //         if (d.name === name) {
-        //             return +d.spend;
-        //         } else {
-        //             return 0;
-        //         }
-        //     });
-        // }; 
-        
-        // var compositeChart = dc.compositeChart('#chart-here');
-        // compositeChart
-        //     .width(990)
-        //     .height(200)
-        //     .dimension(date_dim)
-        //     .x(d3.time.scale().domain([minDate, maxDate]))
-        //     .yAxisLabel("Spend")
-        //     .legend(dc.legend().x(80).y(20).itemHeight(13).gap(5))
-        //     .renderHorizontalGridLines(true)
-        //     .compose([
-        //         dc.lineChart(compositeChart)
-        //             .colors('green')
-        //             .group(tomSpendByMonth, 'Tom'),
-        //         dc.lineChart(compositeChart)
-        //             .colors('red')
-        //             .group(bobSpendByMonth, 'Bob'),
-        //         dc.lineChart(compositeChart)
-        //             .colors('blue')
-        //             .group(aliceSpendByMonth, 'Alice')
-        //     ])
-        //     .brushOn(false)
-        // }  
+        var weapon_chart = dc.barChart("#weapons-display");
+
+        weapon_chart
+            .width(500)
+            .height(300)
+            .margins({top: 10, right: 50, bottom: 30, left: 50})
+            .dimension(weapon_dim)
+            .group(people_killed_group)
+            .valueAccessor(function (p) {
+                return p.value.average;
+            })
+            .transitionDuration(500)
+            .x(d3.scale.ordinal())
+            .xUnits(dc.units.ordinal)
+            .elasticY(true)
+            .xAxisLabel("Attack Type")
+            .yAxis().ticks(4);
+
     
         dc.renderAll();
     }
